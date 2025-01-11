28e2462ab90cd434fad24252afc15dd55cfd047c
mugni1 <asepabdulmugni22@gmail.com>
2025-01-11 10:38:40 +0700

    modified

--- src/app/products/loading.jsx
+++
@@ -1,3 +0,0 @@
-export default function loading() {
-  return <h1 className="py-20">Loading...</h1>;
-}
--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,7 +1,68 @@
+"use client";
+import { useEffect, useState } from "react";
+import { NextRequest } from "next/server";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+
 export default function Products() {
+  let url = new URL("https://fakestoreapi.com/products");
+
+  // state
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  //search params
+  const searchParams = useSearchParams();
+  const limit = searchParams.get("limit");
+  if (limit != null) {
+    url = new URL(`https://fakestoreapi.com/products?limit=${limit}`);
+  }
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: url,
+    })
+      .then((res) => {
+        setProducts(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <main className="h-screen container mx-auto pt-20 grid grid-cols-5 gap-5">
-      <h1>INi Products</h1>
+    <main className=" container mx-auto pt-24 grid grid-cols-5 gap-5">
+      {/* card  */}
+      {loading ? (
+        <h1>Lading ...</h1>
+      ) : (
+        products.map((product) => (
+          <div key={product.id} className="p-5 border shadow-lg rounded-md">
+            <div className="w-full h-40 p-2 border rounded-md">
+              <img src={product.image} className="h-full mx-auto" alt="" />
+            </div>
+            <div>
+              <h1 className="font-bold text-xl text-nowrap text-ellipsis overflow-hidden">
+                Ini Judul
+              </h1>
+              <p className="line-clamp-3">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+                accusamus molestias dolor aliquid ipsam fugit eos blanditiis
+                ducimus dicta aliquam. Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Omnis eius quae, possimus repudiandae odit aut
+                voluptate vero exercitationem sequi dolorem.
+              </p>
+            </div>
+          </div>
+        ))
+      )}
+
+      {/* end card  */}
     </main>
   );
 }
