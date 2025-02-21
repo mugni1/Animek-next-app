59cb6d845c6ef0367ea16f1589cc6f3a39cbbee6
mugni1 <asepabdulmugni22@gmail.com>
2025-02-21 22:48:57 +0700

    add list anime

--- src/app/HomeContent.jsx
+++ src/app/HomeContent.jsx
@@ -45,6 +45,12 @@ export default function HomeContentPage() {
 
   return (
     <Container>
+      <header className="text-3xl font-semibold w-full text-center my-5 flex flex-col gap-5">
+        <span className="bg-gradient-to-br  from-green-400 to-emerald-600 text-transparent bg-clip-text">
+          WATCHING ANIMEK
+        </span>
+        <hr />
+      </header>
       <nav className=" w-full overflow-x-auto flex gap-5 scrollbar-hide mb-5">
         <Btn
           className={`${
--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="bg-slate-900 text-white">
-        <header className="text-3xl font-semibold w-full text-center my-5 flex flex-col gap-5">
-          <span className="bg-gradient-to-br  from-green-400 to-emerald-600 text-transparent bg-clip-text">
-            WATCHING ANIMEK
-          </span>
-          <hr />
-        </header>
         {children}
         <Navbar />
       </body>
