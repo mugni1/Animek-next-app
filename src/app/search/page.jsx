9770533a2dbdfa4ad02d0984531c88b77387e2a2
mugni1 <asepabdulmugni22@gmail.com>
2025-02-23 00:26:05 +0700

    add

---
+++ src/app/search/page.jsx
@@ -0,0 +1,13 @@
+import Container from "@/components/ui/Container";
+
+export default function SearchPage() {
+  return (
+    <Container>
+      <section className="h-full min-h-screen flex justify-center items-center">
+        <h1 className="font-semibold text-2xl w-full text-center mb-36">
+          Cooming Soon
+        </h1>
+      </section>
+    </Container>
+  );
+}
