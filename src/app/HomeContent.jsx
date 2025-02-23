6973f351b452acf76a957f7ee0bf0a6c44b152ef
mugni1 <asepabdulmugni22@gmail.com>
2025-02-23 19:36:44 +0700

    update

--- src/app/HomeContent.jsx
+++ src/app/HomeContent.jsx
@@ -16,43 +16,96 @@ export default function HomeContentPage() {
       {/* Header  */}
       <HomeHeader />
       {/* End Header  */}
+
+      {/* Navigasi panel */}
       <nav className=" w-full overflow-x-auto flex gap-5 scrollbar-hide mb-5">
         <Btn
           className={`${
-            navigate == "recommend" &&
-            "bg-gradient-to-br from-green-400 to-sky-600  "
+            navigate == "recommend"
+              ? "bg-gradient-to-br from-green-400 to-sky-600 border-emerald-600"
+              : "text-white"
           }`}
           onClick={() => setNavigate("recommend")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path d="M7.493 18.5c-.425 0-.82-.236-.975-.632A7.48 7.48 0 0 1 6 15.125c0-1.75.599-3.358 1.602-4.634.151-.192.373-.309.6-.397.473-.183.89-.514 1.212-.924a9.042 9.042 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75A.75.75 0 0 1 15 2a2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H14.23c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23h-.777ZM2.331 10.727a11.969 11.969 0 0 0-.831 4.398 12 12 0 0 0 .52 3.507C2.28 19.482 3.105 20 3.994 20H4.9c.445 0 .72-.498.523-.898a8.963 8.963 0 0 1-.924-3.977c0-1.708.476-3.305 1.302-4.666.245-.403-.028-.959-.5-.959H4.25c-.832 0-1.612.453-1.918 1.227Z" />
+          </svg>
           Recomendation
         </Btn>
         <Btn
           className={`${
-            navigate == "new" && "bg-gradient-to-br from-green-400 to-sky-600  "
+            navigate == "new"
+              ? "bg-gradient-to-br from-green-400 to-sky-600 border-emerald-600"
+              : "text-white"
           }`}
           onClick={() => setNavigate("new")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 6a.75.75 0 0 0-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 0 0 0-1.5h-3.75V6Z"
+              clipRule="evenodd"
+            />
+          </svg>
           New Release
         </Btn>
         <Btn
           className={`${
-            navigate == "ongoing" &&
-            "bg-gradient-to-br from-green-400 to-sky-600 "
+            navigate == "ongoing"
+              ? "bg-gradient-to-br from-green-400 to-sky-600 border-emerald-600"
+              : "text-white"
           }`}
           onClick={() => setNavigate("ongoing")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 0 1 .75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 0 1 9.75 22.5a.75.75 0 0 1-.75-.75v-4.131A15.838 15.838 0 0 1 6.382 15H2.25a.75.75 0 0 1-.75-.75 6.75 6.75 0 0 1 7.815-6.666ZM15 6.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z"
+              clipRule="evenodd"
+            />
+            <path d="M5.26 17.242a.75.75 0 1 0-.897-1.203 5.243 5.243 0 0 0-2.05 5.022.75.75 0 0 0 .625.627 5.243 5.243 0 0 0 5.022-2.051.75.75 0 1 0-1.202-.897 3.744 3.744 0 0 1-3.008 1.51c0-1.23.592-2.323 1.51-3.008Z" />
+          </svg>
           Ongoing
         </Btn>
         <Btn
           className={`${
-            navigate == "completed" &&
-            "bg-gradient-to-br from-green-400 to-sky-600  "
+            navigate == "completed"
+              ? "bg-gradient-to-br from-green-400 to-sky-600 border-emerald-600"
+              : "text-white"
           }`}
           onClick={() => setNavigate("completed")}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+              clipRule="evenodd"
+            />
+          </svg>
           Completed
         </Btn>
       </nav>
+      {/* edn navigasi panel  */}
 
       {/* NAVIGASI  */}
       {navigate === "recommend" && <AnimeRecommend />}
--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -2,7 +2,7 @@ export default function Btn({ className, children, onClick = () => {} }) {
   return (
     <button
       onClick={onClick}
-      className={`py-2 px-5 font-semibold transition-all duration-500 ease-in-out rounded-xl border border-emerald-600 text-nowrap ${className}`}
+      className={`py-2 px-5 font-semibold transition-all ease-in-out rounded-xl border text-nowrap flex items-center gap-1 ${className}`}
     >
       {children}
     </button>
