5cd95a059e6d2f1d072902efdbc598a3c5e40052
mugni1 <asepabdulmugni22@gmail.com>
2025-02-24 20:41:04 +0700

    modified loading from play episode

--- src/app/play-episode/[slug]/Loading.jsx
+++ src/app/play-episode/[slug]/Loading.jsx
@@ -2,46 +2,42 @@ export default function LoadingEpisode() {
   return (
     <section className=" w-full flex flex-col gap-1  scrollbar-hide">
       {/* playyer  */}
-      <div className="w-full h-52 rounded-xl  scrollbar-hide bg-slate-600"></div>
+      <div className="w-full h-52 rounded-xl  scrollbar-hide bg-slate-600 animate-pulse"></div>
       {/* edn playyer  */}
 
       {/* title  */}
-      <h1 className="w-full bg-slate-600 h-8 rounded-xl line-clamp-1 mb-4"></h1>
+      <h1 className="w-full bg-slate-600 animate-pulse h-8 rounded-xl line-clamp-1 mb-4"></h1>
       {/* end title  */}
 
       {/* list server  */}
       <div className="w-full flex flex-col gap-2 mb-4">
-        <h2 className="w-1/2 rounded-xl h-6 bg-slate-600"></h2>
+        <h2 className="w-1/2 rounded-xl h-6 bg-slate-600 animate-pulse"></h2>
         <section className="grid grid-cols-3 w-full gap-4">
-          <button className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600"></button>
-          <button className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600"></button>
-          <button className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600"></button>
-          <button className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600"></button>
-          <button className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600"></button>
-          <button className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600"></button>
+          {Array.from({ length: 12 }).map((_, index) => (
+            <button
+              key={index}
+              className="columns-1 py-1 px-3 h-6 rounded-full bg-slate-600 animate-pulse"
+            ></button>
+          ))}
         </section>
       </div>
       {/* edn list server */}
 
       {/* information  */}
-      <h2 className="font-semibold flex gap-1 h-6 rounded-xl bg-slate-600 w-1/2 mb-1"></h2>
+      <h2 className="font-semibold flex gap-1 h-6 rounded-xl bg-slate-600 animate-pulse w-1/2 mb-1"></h2>
       <section className="flex w-full gap-5 items-start mb-4">
-        <div className="w-4/12 h-40 bg-slate-600 rounded-xl"></div>
-        <div className="w-8/12 h-40 bg-slate-600 rounded-xl"></div>
+        <div className="w-4/12 h-40 bg-slate-600 animate-pulse rounded-xl"></div>
+        <div className="w-8/12 h-40 bg-slate-600 animate-pulse rounded-xl"></div>
       </section>
       {/* end information  */}
 
-      <h2 className="font-semibold flex w-1/2 h-6 bg-slate-600 rounded-xl mb-1"></h2>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify h-5 w-full bg-slate-600 rounded-full"></span>
-      <span className="text-justify mb-20 h-5 w-full bg-slate-600 rounded-full"></span>
+      <h2 className="font-semibold flex w-1/2 h-6 bg-slate-600 animate-pulse rounded-xl mb-1"></h2>
+      {Array.from({ length: 10 }).map((_, index) => (
+        <span
+          key={index}
+          className="text-justify h-6 w-full bg-slate-600 animate-pulse rounded-full"
+        ></span>
+      ))}
     </section>
   );
 }
--- src/app/release-schedule/loading.jsx
+++ src/app/release-schedule/loading.jsx
@@ -3,54 +3,14 @@ import Container from "@/components/ui/Container";
 export default function loadingPage() {
   return (
     <Container>
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
-
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
-
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
-
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
-
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
-
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
-
-      <main className="mb-5 w-full">
-        <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600"></h1>
-        <div className="flex flex-col w-full gap-5">
-          <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 h-28 backdrop-blur-sm overflow-hidden"></section>
-        </div>
-      </main>
+      {Array.from({ length: 7 }).map((_, index) => (
+        <main key={index} className="mb-5 w-full">
+          <h1 className=" rounded-xl mb-3 w-6/12 h-8 bg-slate-600 animate-pulse"></h1>
+          <div className="flex flex-col w-full gap-5">
+            <section className="rounded-xl flex flex-col gap-5 py-3 pe-3 bg-slate-600 animate-pulse h-28 backdrop-blur-sm overflow-hidden"></section>
+          </div>
+        </main>
+      ))}
     </Container>
   );
 }
